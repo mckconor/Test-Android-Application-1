6f99394752cf7596ddd5e704f14ca4b1de5dabb1
mckennc9 <mckennc9@tcd.ie>
2017-09-27 13:35:31 +0100

    Initial Commit
    Very simple testing, not fully working yet.

---
+++ app/src/main/java/com/example/mckennc9/testapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mckennc9.testapplication;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
+    }
+
+    private void addHeatMap() {
+        List<LatLng> listOfLocations = null;
+
+        //Get User locations
+    }
+}
